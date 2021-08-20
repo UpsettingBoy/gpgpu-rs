945a21c8192a855935e13b06658aa8ef76b008a3
Jerónimo Sánchez <JeronimoSG@hotmail.es>
2021-08-20 11:35:46 +0000

    Created basic Framework implementation + Implemented buffer creation (storage and staging) + Implemented kernel creation (OpenCL equivalent) + Implemented device polling fns

---
+++ src/framework.rs
@@ -0,0 +1,137 @@
+use std::{marker::PhantomData, sync::Arc};
+
+use wgpu::util::DeviceExt;
+
+use crate::{Framework, GpuBuffer, Kernel};
+
+impl Default for Framework {
+    fn default() -> Self {
+        let instance = wgpu::Instance::new(wgpu::Backends::PRIMARY);
+        let (device, queue) = futures::executor::block_on(async {
+            let adapter = instance
+                .request_adapter(&wgpu::RequestAdapterOptions {
+                    power_preference: wgpu::PowerPreference::HighPerformance,
+                    ..Default::default()
+                })
+                .await
+                .unwrap();
+
+            adapter
+                .request_device(
+                    &wgpu::DeviceDescriptor {
+                        label: None,
+                        features: wgpu::Features::empty(),
+                        limits: wgpu::Limits::downlevel_defaults(),
+                    },
+                    None,
+                )
+                .await
+                .unwrap()
+        });
+
+        Self {
+            instance,
+            device,
+            queue,
+        }
+    }
+}
+
+impl Framework {
+    pub fn new(instance: wgpu::Instance, device: wgpu::Device, queue: wgpu::Queue) -> Self {
+        Self {
+            instance,
+            device,
+            queue,
+        }
+    }
+
+    pub fn create_kernel(
+        &self,
+        shader: Arc<wgpu::ShaderModule>,
+        entry_point: impl Into<String>,
+    ) -> Kernel {
+        let entry_point: String = entry_point.into();
+        let name = format!("{:?}::{}", shader, &entry_point);
+
+        let pipeline = self
+            .device
+            .create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+                label: Some(&name),
+                entry_point: &entry_point,
+                layout: None,
+                module: &shader,
+            });
+
+        Kernel {
+            shader,
+            name,
+            entry_point,
+            pipeline,
+            descriptors: Vec::new(),
+        }
+    }
+
+    pub fn create_buffer<T>(&self, len: usize) -> GpuBuffer<T>
+    where
+        T: bytemuck::Pod,
+    {
+        let size = len * std::mem::size_of::<T>();
+
+        let storage = self.device.create_buffer(&wgpu::BufferDescriptor {
+            label: None,
+            size: size as u64,
+            usage: wgpu::BufferUsages::STORAGE
+                | wgpu::BufferUsages::COPY_SRC
+                | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        GpuBuffer {
+            storage,
+            size,
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn create_buffer_from_slice<T>(&self, data: &[T]) -> GpuBuffer<T>
+    where
+        T: bytemuck::Pod,
+    {
+        let size = data.len() * std::mem::size_of::<T>();
+
+        let storage = self
+            .device
+            .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: None,
+                contents: bytemuck::cast_slice(data),
+                usage: wgpu::BufferUsages::STORAGE
+                    | wgpu::BufferUsages::COPY_SRC
+                    | wgpu::BufferUsages::COPY_DST,
+            });
+
+        GpuBuffer {
+            storage,
+            size,
+            _marker: PhantomData,
+        }
+    }
+
+    // TODO: Reuse staging buffers from pool instead of creating-destroying for every read
+    pub(crate) fn create_staging_buffer(&self, size: usize) -> wgpu::Buffer {
+        self.device.create_buffer(&wgpu::BufferDescriptor {
+            label: None,
+            size: size as u64,
+            usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        })
+    }
+
+    pub fn poll(&self) {
+        self.device.poll(wgpu::Maintain::Poll);
+    }
+
+    pub fn poll_wait(&self) {
+        self.device.poll(wgpu::Maintain::Wait);
+    }
+}
