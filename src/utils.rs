bf1858173e3b2addb422b7ec047c31e4d3b9e4a3
Jerónimo Sánchez <jeronimosg@hotmail.es>
2021-08-28 20:29:07 +0200

    Changed Arc to borrow

--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,11 @@
 pub mod shader {
-    use std::{path::Path, sync::Arc};
+    use std::path::Path;
 
     use wgpu::ShaderModule;
 
     use crate::{Framework, GpuResult};
 
-    pub fn from_spirv_file(fw: Arc<Framework>, path: impl AsRef<Path>) -> GpuResult<ShaderModule> {
+    pub fn from_spirv_file(fw: &Framework, path: impl AsRef<Path>) -> GpuResult<ShaderModule> {
         let bytes = std::fs::read(&path)?;
         let shader_name = path.as_ref().to_str();
 
@@ -13,7 +13,7 @@ pub mod shader {
     }
 
     pub fn from_spirv_bytes(
-        fw: Arc<Framework>,
+        fw: &Framework,
         bytes: &[u8],
         shader_name: Option<&str>,
     ) -> ShaderModule {
