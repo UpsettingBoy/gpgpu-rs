f18ad4fa0b89492552ce3e661d8ae3e48b04e982
Jerónimo Sánchez <jeronimosg@hotmail.es>
2021-09-19 12:25:48 +0200

    Creation of `GpuBuffer::write()` and `GpuImage::read_async()`
    TODO: Write operations for both `GpuBuffer` and `GpuImage` are immediate.
    So, it could be useful to provide a `write_derefered()` of similar method
    that doesn't create an encoder so multiple writes to different objects
    can be enqueued and executed at will with a `flush_writes()` or similar
    function.

--- src/alloc.rs
+++ src/alloc.rs
@@ -81,6 +81,22 @@ where
 
         Ok(result)
     }
+
+    /// Writes the `data` information into the [`GpuBuffer`] immediately.
+    pub fn write(&mut self, data: &[T]) {
+        self.fw
+            .queue
+            .write_buffer(&self.storage, 0, bytemuck::cast_slice(data));
+
+        let encoder = self
+            .fw
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Buffer write"),
+            });
+
+        self.fw.queue.submit(Some(encoder.finish()));
+    }
 }
 
 impl<'fw> GpuImage<'fw> {
@@ -89,26 +105,56 @@ impl<'fw> GpuImage<'fw> {
         wgpu::BindingResource::TextureView(&self.full_view)
     }
 
-    /// Writes the `img_bytes` bytes into the [`GpuImage`] in the
-    /// format specified at its creation.
-    pub fn write(&mut self, img_bytes: &[u8]) {
+    /// Asyncronously reads the contents of the [`GpuImage`] into a [`Vec`].
+    ///
+    /// In order for this future to resolve, [`Framework::poll`] or [`Framework::blocking_poll`]
+    /// must be invoked.
+    pub async fn read_async(&self) -> GpuResult<Vec<u8>> {
         use std::num::NonZeroU32;
 
-        self.fw.queue.write_texture(
-            wgpu::ImageCopyTexture {
-                texture: &self.texture,
-                mip_level: 0,
-                origin: wgpu::Origin3d::ZERO,
-                aspect: wgpu::TextureAspect::All,
-            },
-            img_bytes,
-            wgpu::ImageDataLayout {
+        let staging = self
+            .fw
+            .create_staging_buffer((4 * self.size.width * self.size.height) as usize);
+
+        let mut encoder = self
+            .fw
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Image copy"),
+            });
+
+        let copy_texture = wgpu::ImageCopyTexture {
+            aspect: wgpu::TextureAspect::All,
+            mip_level: 0,
+            origin: wgpu::Origin3d::ZERO,
+            texture: &self.texture,
+        };
+
+        let copy_buffer = wgpu::ImageCopyBuffer {
+            buffer: &staging,
+            layout: wgpu::ImageDataLayout {
                 offset: 0,
-                bytes_per_row: NonZeroU32::new(4 * self.size.width), // TODO: change 4 for img format pixel byte size
+                bytes_per_row: NonZeroU32::new(4 * self.size.width),
                 rows_per_image: NonZeroU32::new(self.size.height),
             },
-            self.size,
-        );
+        };
+
+        encoder.copy_texture_to_buffer(copy_texture, copy_buffer, self.size);
+
+        self.fw.queue.submit(Some(encoder.finish()));
+
+        let buff_slice = staging.slice(..);
+        let buf_future = buff_slice.map_async(wgpu::MapMode::Read);
+
+        buf_future.await?;
+
+        let data = buff_slice.get_mapped_range();
+        let result = bytemuck::cast_slice(&data).to_vec();
+
+        drop(data);
+        staging.unmap();
+
+        Ok(result)
     }
 
     /// Blocking read of the content of the [`GpuImage`] into a [`Vec`].
@@ -160,5 +206,36 @@ impl<'fw> GpuImage<'fw> {
         staging.unmap();
 
         Ok(result)
+    }
+
+    /// Writes the `img_bytes` bytes into the [`GpuImage`] immediately.
+    /// The image is format specified at the [`GpuImage`] creation.
+    pub fn write(&mut self, img_bytes: &[u8]) {
+        use std::num::NonZeroU32;
+
+        self.fw.queue.write_texture(
+            wgpu::ImageCopyTexture {
+                texture: &self.texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+                aspect: wgpu::TextureAspect::All,
+            },
+            img_bytes,
+            wgpu::ImageDataLayout {
+                offset: 0,
+                bytes_per_row: NonZeroU32::new(4 * self.size.width), // TODO: change 4 for img format pixel byte size
+                rows_per_image: NonZeroU32::new(self.size.height),
+            },
+            self.size,
+        );
+
+        let encoder = self
+            .fw
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Buffer write"),
+            });
+
+        self.fw.queue.submit(Some(encoder.finish()));
     }
 }
