ca3aab2780f2095d95809e1c7ee8a049af93b7ad
UpsettingBoy <jeronimosg@hotmail.es>
2021-10-30 17:48:41 +0200

    Added GpuUniformBuffer write methods

--- src/primitives/buffer.rs
+++ src/primitives/buffer.rs
@@ -205,7 +205,6 @@ where
             label: None,
             size: size as u64,
             usage: wgpu::BufferUsages::UNIFORM
-                | wgpu::BufferUsages::COPY_SRC
                 | wgpu::BufferUsages::COPY_DST,
             mapped_at_creation: false,
         });
@@ -230,7 +229,6 @@ where
                 label: None,
                 contents: bytemuck::cast_slice(data),
                 usage: wgpu::BufferUsages::UNIFORM
-                    | wgpu::BufferUsages::COPY_SRC
                     | wgpu::BufferUsages::COPY_DST,
             });
 
@@ -240,5 +238,73 @@ where
             size,
             _marker: PhantomData,
         }
+    }
+
+    /// Creates a complete [`BindingResource`](wgpu::BindingResource) of the [`GpuUniformBuffer`].
+    pub fn as_binding_resource(&self) -> wgpu::BindingResource {
+        self.storage.as_entire_binding()
+    }
+
+    /// Obtains the number of elements (or capacity if created using [`Framework::create_buffer`](crate::Framework::create_buffer))
+    /// of the [`GpuUniformBuffer`].
+    pub fn len(&self) -> usize {
+        self.size / std::mem::size_of::<T>()
+    }
+
+    /// Checks if the [`GpuUniformBuffer`] is empty.
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /// Obtains the size in bytes of this [`GpuUniformBuffer`].
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    /// Asyncronously writes the contents of `data` into the [`GpuUniformBuffer`].
+    ///
+    /// In order for this future to resolve, [`Framework::poll`](crate::Framework::poll) or [`Framework::blocking_poll`](crate::Framework::blocking_poll)
+    /// must be invoked.
+    pub async fn write_async(&mut self, data: &[T]) -> GpuResult<()> {
+        let staging = self.fw.create_upload_staging_buffer(self.size);
+
+        let mut encoder = self
+            .fw
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("GpuUniformBuffer::write_async"),
+            });
+        encoder.copy_buffer_to_buffer(&staging, 0, &self.storage, 0, self.size as u64);
+
+        self.fw.queue.submit(Some(encoder.finish()));
+
+        let buff_slice = self.storage.slice(..);
+        let buf_future = buff_slice.map_async(wgpu::MapMode::Write);
+
+        buf_future.await?;
+
+        let mut write_view = buff_slice.get_mapped_range_mut();
+        write_view.copy_from_slice(bytemuck::cast_slice(data));
+
+        drop(write_view);
+        self.storage.unmap();
+
+        Ok(())
+    }
+
+    /// Writes the `data` information into the [`GpuUniformBuffer`] immediately.
+    pub fn write(&mut self, data: &[T]) {
+        self.fw
+            .queue
+            .write_buffer(&self.storage, 0, bytemuck::cast_slice(data));
+
+        let encoder = self
+            .fw
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("GpuUniformBuffer::write"),
+            });
+
+        self.fw.queue.submit(Some(encoder.finish()));
     }
 }
