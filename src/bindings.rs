93fd710231ea12eb1f3fa9d5469801c76ed563b5
myname1111 <animationstudiocp@gmail.com>
2023-04-20 16:57:06 +0700

    Revert "Use ASBindingResource in SetBindings to decrease boilerplate"
    This reverts commit 9a6404e25eff18c3158541fea2596f28a8affac6.

--- src/bindings.rs
+++ src/bindings.rs
@@ -1,4 +1,4 @@
-use crate::primitives::*;
+use crate::{primitives::*, *};
 
 #[derive(Clone, Default)]
 pub struct SetBindings<'res> {
@@ -6,10 +6,43 @@ pub struct SetBindings<'res> {
 }
 
 impl<'res> SetBindings<'res> {
-    pub fn add_entry<T: AsBindingResource>(&mut self, bind_id: u32, bind: &'res T) {
+    pub fn add_buffer<T>(&mut self, bind_id: u32, buffer: &'res GpuBuffer<T>)
+    where
+        T: bytemuck::Pod,
+    {
         let bind = wgpu::BindGroupEntry {
             binding: bind_id,
-            resource: bind.as_binding_resource(),
+            resource: buffer.as_binding_resource(),
+        };
+
+        self.bindings.push(bind)
+    }
+
+    pub fn add_uniform_buffer<T>(&mut self, bind_id: u32, buffer: &'res GpuUniformBuffer<T>)
+    where
+        T: bytemuck::Pod,
+    {
+        let bind = wgpu::BindGroupEntry {
+            binding: bind_id,
+            resource: buffer.as_binding_resource(),
+        };
+
+        self.bindings.push(bind)
+    }
+
+    pub fn add_image<P: PixelInfo>(&mut self, bind_id: u32, img: &'res GpuImage<P>) {
+        let bind = wgpu::BindGroupEntry {
+            binding: bind_id,
+            resource: img.as_binding_resource(),
+        };
+
+        self.bindings.push(bind)
+    }
+
+    pub fn add_const_image<P: PixelInfo>(&mut self, bind_id: u32, img: &'res GpuConstImage<P>) {
+        let bind = wgpu::BindGroupEntry {
+            binding: bind_id,
+            resource: img.as_binding_resource(),
         };
 
         self.bindings.push(bind)
