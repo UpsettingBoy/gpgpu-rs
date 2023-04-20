225a4c64e0a01b9507b5d60016c485daf6f779e6
myname1111 <animationstudiocp@gmail.com>
2023-04-20 14:44:01 +0700

    add new_set_layout macro

--- src/layout.rs
+++ src/layout.rs
@@ -5,6 +5,43 @@ pub struct SetLayout {
     pub(crate) layout_entry: Vec<wgpu::BindGroupLayoutEntry>,
 }
 
+/// Creates a new [SetLayout]
+///
+/// Usage:
+/// ```
+/// use gpgpu::{new_set_layout, primitives::pixels::Rgba8Uint, BufOps};
+/// new_set_layout!(
+///     0: UniformBuffer,
+///     1: Image<Rgba8Uint>, // Replace Rgba8Uint with anything that implements [PixelInfo]
+///     2: ConstImage<Rgba8Uint>,
+///     3: Buffer(gpgpu::GpuBufferUsage::ReadOnly)
+/// );
+/// ```
+#[macro_export]
+macro_rules! new_set_layout {
+    (@add_entry $usage:expr, $layout:expr, $id:literal, Buffer) => {
+        $layout.add_buffer($id, $usage);
+    };
+    (@add_entry $layout:expr, $id:literal, UniformBuffer) => {
+        $layout.add_uniform_buffer($id);
+    };
+    (@add_entry $p:ty, $layout:expr, $id:literal, Image) => {
+        $layout.add_image::<$p>($id);
+    };
+    (@add_entry $p:ty, $layout:expr, $id:literal, ConstImage) => {
+        $layout.add_const_image::<$p>($id);
+    };
+    ($($id:literal: $ty:tt$(<$p:ty>)?$(($usage:expr))?),+) => {{
+        let mut layout = $crate::layout::SetLayout::default();
+
+        $(
+            $crate::new_set_layout!(@add_entry $($p, )? $($usage, )? layout, $id, $ty);
+        )+
+
+        layout
+    }};
+}
+
 impl SetLayout {
     pub fn add_buffer(&mut self, bind_id: u32, usage: GpuBufferUsage) {
         let entry = wgpu::BindGroupLayoutEntry {
