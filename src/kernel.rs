2e4bbfbf817e6c5b029870b6af926028b3926ca2
myname1111 <animationstudiocp@gmail.com>
2023-04-20 19:10:00 +0700

    Make kernel use SetLayout for better performace

--- src/kernel.rs
+++ src/kernel.rs
@@ -6,39 +6,43 @@ use crate::*;
 pub struct Kernel<'fw> {
     fw: &'fw Framework,
     pipeline: wgpu::ComputePipeline,
-    sets: Vec<wgpu::BindGroup>,
-    entry_point: String,
+    // sets: Vec<wgpu::BindGroup>,
+    // entry_point: String,
 }
 
 impl<'fw> Kernel<'fw> {
     /// Creates a [`Kernel`] from a [`Program`].
-    pub fn new<'sha, 'res>(fw: &'fw Framework, program: Program<'sha, 'res>) -> Self {
-        let mut layouts = Vec::new();
-        let mut sets = Vec::new();
-
-        // Unwraping of descriptors from program
-        for (set_id, desc) in program.descriptors.iter().enumerate() {
-            let set_layout = fw
-                .device
-                .create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                    label: None,
-                    entries: &desc.set_layout,
-                });
-
-            let set = fw.device.create_bind_group(&wgpu::BindGroupDescriptor {
-                label: None,
-                layout: &set_layout,
-                entries: &desc.binds,
-            });
-
-            log::debug!("Binding set = {} with {:#?}", set_id, &desc.binds);
-
-            layouts.push(set_layout);
-            sets.push(set);
-        }
+    pub fn new<'sha, 'res>(
+        fw: &'fw Framework,
+        shader: &'sha Shader,
+        entry_point: impl Into<String>,
+        layouts: Vec<SetLayout>,
+    ) -> Self {
+        // let set = fw.device.create_bind_group(&wgpu::BindGroupDescriptor {
+        //     label: None,
+        //     layout: &set_layout,
+        //     entries: &desc.binds,
+        // });
+        //
+        // log::debug!("Binding set = {} with {:#?}", set_id, &desc.binds);
 
         // Compute pipeline bindings
-        let group_layouts = layouts.iter().collect::<Vec<_>>();
+        let layouts = layouts
+            .iter()
+            .map(|layout| {
+                fw.device
+                    .create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                        label: None,
+                        entries: &layout.layout_entry,
+                    })
+            })
+            .collect::<Vec<_>>();
+
+        let mut group_layouts = vec![];
+
+        for layout in layouts.iter() {
+            group_layouts.push(layout)
+        }
 
         let pipeline_layout = fw
             .device
@@ -52,44 +56,40 @@ impl<'fw> Kernel<'fw> {
             .device
             .create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
                 label: None,
-                module: &program.shader.0,
-                entry_point: &program.entry_point,
+                module: &shader.0,
+                entry_point: &entry_point.into(),
                 layout: Some(&pipeline_layout),
             });
 
-        Self {
-            fw,
-            pipeline,
-            sets,
-            entry_point: program.entry_point,
-        }
+        Self { fw, pipeline }
     }
 
     /// Enqueues the execution of this [`Kernel`] onto the GPU.
     ///
     /// [`Kernel`] will dispatch `x`, `y` and `z` workgroups per dimension.
     pub fn enqueue(&self, x: u32, y: u32, z: u32) {
-        let mut encoder = self
-            .fw
-            .device
-            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                label: Some("Kernel::enqueue"),
-            });
-        {
-            let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
-                label: Some("Kernel::enqueue"),
-            });
-
-            cpass.set_pipeline(&self.pipeline);
-
-            for (id_set, set) in self.sets.iter().enumerate() {
-                cpass.set_bind_group(id_set as u32, set, &[]);
-            }
-
-            cpass.insert_debug_marker(&self.entry_point);
-            cpass.dispatch_workgroups(x, y, z);
-        }
-
-        self.fw.queue.submit(Some(encoder.finish()));
+        todo!();
+        // let mut encoder = self
+        //     .fw
+        //     .device
+        //     .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+        //         label: Some("Kernel::enqueue"),
+        //     });
+        // {
+        //     let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+        //         label: Some("Kernel::enqueue"),
+        //     });
+        //
+        //     cpass.set_pipeline(&self.pipeline);
+        //
+        //     for (id_set, set) in self.sets.iter().enumerate() {
+        //         cpass.set_bind_group(id_set as u32, set, &[]);
+        //     }
+        //
+        //     cpass.insert_debug_marker(&self.entry_point);
+        //     cpass.dispatch_workgroups(x, y, z);
+        // }
+        //
+        // self.fw.queue.submit(Some(encoder.finish()));
     }
 }
