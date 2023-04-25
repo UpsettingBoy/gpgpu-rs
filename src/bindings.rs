a5f75c04fed11fabd15b1ff6fcb44b3e4bcd9d65
myname1111 <animationstudiocp@gmail.com>
2023-04-25 16:12:49 +0700

    Add docs to SetBindings

--- src/bindings.rs
+++ src/bindings.rs
@@ -1,5 +1,26 @@
+//! Provides the [`SetBindings`] struct used to bind the data to the gpu
 use crate::{entry_type::EntryType, primitives::*, *};
 
+/// Binds data to bindings that will be sent to the gpu
+///
+/// # Example
+/// ```
+/// # use gpgpu::*;
+/// let fw = Framework::default();
+///
+/// let data = (0..10000).into_iter().collect::<Vec<u32>>();
+/// let scalar = 10u32;
+///
+/// // Create the buffer
+/// let buffer = GpuBuffer::from_slice(&fw, &data);
+///
+/// // Create the uniform
+/// let uniform = GpuUniformBuffer::from_slice(&fw, &[scalar]);
+///
+/// let binds = SetBindings::default()
+///     .add_buffer(0, &buffer) // Binds data to the gpu with id 0
+///     .add_uniform_buffer(1, &uniform); // Bind a uniform with id 1
+/// ```
 #[derive(Clone, Default)]
 pub struct SetBindings<'res> {
     pub(crate) bindings: Vec<wgpu::BindGroupEntry<'res>>,
@@ -7,6 +28,15 @@ pub struct SetBindings<'res> {
 }
 
 impl<'res> SetBindings<'res> {
+    /// Binds a new [`GpuBuffer`] with a bind id
+    /// # Example
+    /// ```
+    /// # let fw = Framework::default();
+    /// let data = (0..10000).into_iter().collect::<Vec<u32>>();
+    /// let buffer = GpuBuffer::from_slice(&fw, &data);
+    ///
+    /// SetBindings::default().add_buffer(0, &buffer);
+    /// ```
     pub fn add_buffer<T>(mut self, bind_id: u32, buffer: &'res GpuBuffer<T>) -> Self
     where
         T: bytemuck::Pod,
@@ -22,6 +52,15 @@ impl<'res> SetBindings<'res> {
         self
     }
 
+    /// Binds a new [`GpuUniformBuffer`] with a bind id
+    /// # Example
+    /// ```
+    /// # let fw = Framework::default();
+    /// let scalar = 10u32;
+    /// let uniform = GpuUniformBuffer::from_slice(&fw, &[scalar]);
+    ///
+    /// SetBindings::default().add_uniform_buffer(0, &uniform);
+    /// ```
     pub fn add_uniform_buffer<T>(mut self, bind_id: u32, buffer: &'res GpuUniformBuffer<T>) -> Self
     where
         T: bytemuck::Pod,
@@ -37,6 +76,15 @@ impl<'res> SetBindings<'res> {
         self
     }
 
+    /// Binds a new [`GpuImage`] with a bind id
+    /// # Example
+    /// ```
+    /// # let fw = Framework::default();
+    /// let data = [0, 0, 0];
+    /// let image = GpuImage::from_bytes(&fw, &data, 1, 1);
+    ///
+    /// SetBindings::default().add_image(0, &image);
+    /// ```
     pub fn add_image<P: PixelInfo>(mut self, bind_id: u32, img: &'res GpuImage<P>) -> Self {
         let bind = wgpu::BindGroupEntry {
             binding: bind_id,
@@ -49,6 +97,15 @@ impl<'res> SetBindings<'res> {
         self
     }
 
+    /// Binds a new [`GpuConstImage`] with a bind id
+    /// # Example
+    /// ```
+    /// # let fw = Framework::default();
+    /// let data = [0, 0, 0];
+    /// let image = GpuConstImage::from_bytes(&fw, &data, 1, 1);
+    ///
+    /// SetBindings::default().add_const_image(0, &image);
+    /// ```
     pub fn add_const_image<P: PixelInfo>(
         mut self,
         bind_id: u32,
