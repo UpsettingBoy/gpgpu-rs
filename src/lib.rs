c58489649b0d22e5d25b6aa0e5772e72cb1a4a99
myname1111 <animationstudiocp@gmail.com>
2023-04-25 13:22:55 +0700

    Update documentation in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -89,13 +89,13 @@ mod entry_type;
 
 #[derive(PartialEq, Eq)]
 pub enum GpuBufferUsage {
-    /// Read-only object.
+    /// Read-only buffer.
     /// ### Example WGSL syntax:
     /// ```ignore
     /// [[group(0), binding(0)]] var<storage, read> input: Vector;
     /// ```
     ReadOnly,
-    /// Read-write object.
+    /// Read-write buffer.
     /// ### Example WGSL syntax:
     /// ```ignore
     /// [[group(0), binding(0)]] var<storage, read_write> input: Vector;
@@ -108,8 +108,22 @@ pub enum GpuBufferUsage {
 ///
 /// Equivalent to OpenCL's Buffer objects.
 ///
-/// More information about its shader representation is
-/// under the [`DescriptorSet::bind_buffer`](crate::DescriptorSet::bind_buffer) documentation.
+/// ### Example WGSL syntax:
+/// ```ignore
+/// struct StorageStruct {
+///     data: [[stride(4)]] array<i32>;
+/// };
+///
+/// [[group(0), binding(0)]]
+/// var<storage, read_write> myStorageBuffer: StorageStruct;
+/// ```
+///
+/// ### Example GLSL syntax:
+/// ```glsl
+/// layout (set=0, binding=0) buffer myStorageBuffer {
+///     int data[];
+/// };
+/// ```
 pub struct GpuBuffer<'fw, T> {
     fw: &'fw Framework,
     buf: wgpu::Buffer,
@@ -123,8 +137,27 @@ pub struct GpuBuffer<'fw, T> {
 ///
 /// Equivalent to OpenCL's Uniform Buffer objects.
 ///
-/// More information about its shader representation is
-/// under the [`DescriptorSet::bind_uniform_buffer`](crate::DescriptorSet::bind_uniform_buffer) documentation.
+/// ### Example WGSL syntax:
+/// ```ignore
+/// struct UniformStruct {
+///     a: vec3<u32>;
+///     b: vec3<u32>;
+///     c: vec3<u32>;
+/// };
+///
+/// [[group(0), binding(0)]]
+/// var<uniform> myUniformBuffer: UniformStruct;
+/// ```
+///
+/// ### Example GLSL syntax:
+/// ```glsl
+/// layout(std140, binding = 0)
+/// uniform UniformStruct {
+///     uvec3 a;
+///     uvec3 b;
+///     uvec3 c;
+/// };
+/// ```
 pub struct GpuUniformBuffer<'fw, T> {
     fw: &'fw Framework,
     buf: wgpu::Buffer,
@@ -136,8 +169,17 @@ pub struct GpuUniformBuffer<'fw, T> {
 ///
 /// Equivalent to write-only OpenCL's Image objects.
 ///
-/// More information about its shader representation is
-/// under the [`DescriptorSet::bind_image`](crate::DescriptorSet::bind_image) documentation.
+/// This image is write-only.
+/// ### Example WGSL syntax:
+/// ```ignore
+/// [[group(0), binding(0)]]
+/// var myStorageImg: texture_storage_2d<rgba8uint, write>;
+/// ```
+///
+/// ### Example GLSL syntax:
+/// ```glsl
+/// layout (set=0, binding=0, rgba8uint) uimage2D myStorageImg;
+/// ```
 pub struct GpuImage<'fw, P> {
     fw: &'fw Framework,
     texture: wgpu::Texture,
@@ -150,8 +192,17 @@ pub struct GpuImage<'fw, P> {
 ///
 /// Equivalent to read-only OpenCL's Image objects.
 ///
-/// More information about its shader representation is
-/// under the [`DescriptorSet::bind_const_image`](crate::DescriptorSet::bind_const_image) documentation.
+/// This image is read-only.
+/// ### Example WGSL syntax:
+/// ```ignore
+/// [[group(0), binding(0)]]
+/// var myTexture: texture_2d<u32>;
+/// ```
+///
+/// ### Example GLSL syntax:
+/// ```glsl
+/// layout (set=0, binding=0) utexture2D myTexture;
+/// ```
 pub struct GpuConstImage<'fw, P> {
     fw: &'fw Framework,
     texture: wgpu::Texture,
